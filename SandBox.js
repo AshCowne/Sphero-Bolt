3d40bcdb580d3b0fa3d7930a4543504c1d359391
Cowne <ashcowne@gmail.com>
2024-04-18 14:16:21 -0400

    Added Hello World Code Script From Wiki

--- SandBox.js
+++ SandBox.js
@@ -1,10 +1,14 @@
-// Programer: Ash Cowne
-//Team Member: Preston
-//Bolt ID: 
+// Programer: Ash Cowne  
+//Team Member: Lauren
+//Bolt ID: CF6D
 //Date: 4.16.2024
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700433/edit
 
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
