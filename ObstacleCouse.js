3273b709f9384ba4f1bc22d24177d00439277127
Cowne <ashcowne@gmail.com>
2024-04-24 13:55:15 -0400

    CheckPoint 1

--- ObstacleCouse.js
+++ ObstacleCouse.js
@@ -4,3 +4,18 @@
 //Date: 4.16.2024
 //Program: Hello World - Sandbox
 //Program URL: https://edu.sphero.com/program/16700433/edit
+
+
+// Check Point 1
+// Programmer: Lauryn Meyer
+// Team Member: Ash
+// Bolt ID: SB-CF6D
+// Date: 4-24-2024
+// Program: Obstacle Course (prelim)
+// Program URL: https://edu.sphero.com/program/16752489/edit
+
+// Programmer 1 : Lauryn
+async function startProgram() {
+	await roll(0,70,2.25)	
+	await delay (1);
+}
