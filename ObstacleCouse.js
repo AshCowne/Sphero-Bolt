0b699f2bd9de1859892bbed5e87bf4461af82b52
Cowne <ashcowne@gmail.com>
2024-04-24 14:11:44 -0400

    CheckPoint 2

--- ObstacleCouse.js
+++ ObstacleCouse.js
@@ -7,15 +7,19 @@
 
 
 // Check Point 1
-// Programmer: Lauryn Meyer
-// Team Member: Ash
-// Bolt ID: SB-CF6D
-// Date: 4-24-2024
-// Program: Obstacle Course (prelim)
-// Program URL: https://edu.sphero.com/program/16752489/edit
+// Programmer: Lauryn Meyer :Programer 1
 
-// Programmer 1 : Lauryn
 async function startProgram() {
 	await roll(0,70,2.25)	
+	await delay (1);
+
+
+
+//CheckPoint 2
+// Programer: Ash Cowne  : Programer 2
+
+	await roll((getHeading() - 70), 60, 2.5);
+	await delay (1);
+	await roll((getHeading() - 90), 60, 1);
 	await delay (1);
 }
