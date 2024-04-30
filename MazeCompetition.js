265fb72ead8849099bdaea47305e82dcdcce2768
Cowne <ashcowne@gmail.com>
2024-04-30 13:28:00 -0400

    Added Intial Commints

---
+++ MazeCompetition.js
@@ -0,0 +1,12 @@
+// Programer: Ash Cowne  
+//Team Member: Lauren
+//Bolt ID: CF6D
+//Date: 4.30.2024
+//Program: Maze Competition
+//Program URL: https://edu.sphero.com/program/16788344/edit
+
+
+
+async function startProgram() {
+	// Write code here
+}
