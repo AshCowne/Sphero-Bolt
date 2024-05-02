e11d576405de3810b9271ea162b4abf5a843d1d5
Cowne <ashcowne@gmail.com>
2024-05-02 13:49:02 -0400

    Sound Two Checkpoint Commit and added what we changed yesterday.

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -16,25 +16,35 @@ async function startProgram() {
 
 	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date: 5.1.2024
 
-	await roll((getHeading() + 0), 75, 2.25);
+	await roll((getHeading() + 0), 75, 2);
 	await delay(1);
 	setMainLed({ r: 0, g: 0, b: 100 });
 
 // Programer: Ash Cowne  - Sound One Checkpoint Commit - Date: 5.1.2024
-	await roll((getHeading() + 90), 75,1.4);
+	await roll((getHeading() + 90), 77,1.4);
 	await delay(1);
 	await Sound.Effects.Fairy.play()
 	
 	// Programmer: Lauryn Meyer - Red LED Checkpoint Commit - Date: 5.1.2024
-	await roll((getHeading() + 90), 75,1);
+	await roll((getHeading() + 90), 80,0.8);
 	await delay(1);
-	await roll((getHeading() + 40),75,1);
-	await delay(.5);
+	await roll((getHeading() + 40),70,1);
+	await delay(1);
 	setMainLed({ r: 100, g: 0, b: 0 });
+	await delay(1);
 	
 
 	// Programer: - Sound Two Checkpoint Commit - Date:
+	await roll((getHeading() - 90), 70,0.6);
+	await delay(1);
+	await roll((getHeading() - 90), 75,1);
+	await delay(1);
+	await roll((getHeading() + 90), 60,0.7);
+	await Sound.Ambience.Zoo.play()
+	
 
+	
+	
 
 
 
